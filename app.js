b06bd910429d7cacd1d15df23dad1b5b8547b183
Minh <anhddgch18611@fpt.edu.vn>
2021-05-04 15:47:23 +0700

    commit

--- app.js
+++ app.js
@@ -1,58 +1,70 @@
+/////////////////////
+//express
 const express = require('express');
 const app = express();
+/////////////////////
 
-
-
-var bodyParser = require("body-parser");
-const { parse } = require('path');
-app.use(bodyParser.urlencoded({ extended: false }));
-
-var publicDir = require('path').join(__dirname, '/public');
-app.use(express.static(publicDir));
-
-
+/////////////
+//get views
 app.set('views', './views');
-
+/////////////
+//mongodb
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://minh15:minh1507@cluster0.x1k9j.mongodb.net/test";
-
+var url = "mongodb://localhost:27017";
+/////////////
+//session
 const session = require('express-session');
-
 app.use(session({
     resave: true,
     saveUninitialized: true,
     secret: 'some122$$%*$##!!#$%@#$%',
     cookie: { maxAge: 60000 }
 }));
-
-
+//////////////
+//hbs
 var hbs = require('hbs')
 app.set('view engine', 'hbs')
-
-
+//////////////
+//bodyparser
 var bodyParser = require("body-parser");
 const cons = require('consolidate');
 app.use(bodyParser.urlencoded({ extended: false }))
+///////////////
 
-app.get('/login', (req, res) => {
-    res.render('login')
-})
-app.get('/register', (req, res) => {
-    res.render('register')
-})
+/////////////////////////////////////////////////////////////////////////////////////////
+//not login page
 app.get('/notLogin', (req, res) => {
     res.render('notLogin')
 })
+
+////////////////////
+//regiter
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
 app.post('/new', async (req, res) => {
     var nameInput = req.body.txtName;
     var passInput = req.body.txtPassword;
     var roleInput = req.body.role;
-    var newUser = { name: nameInput, password: passInput, role: roleInput };
-
-    let client = await MongoClient.connect(url);
-    let dbo = client.db("toyshop");
-    await dbo.collection("users").insertOne(newUser);
-    res.redirect('/login')
+    
+    if (nameInput.trim().length < 8)
+    {
+        res.render('register', {nameerr: 'Username must be more than 8 character'})
+    }
+    else
+    {
+            var newUser = { name: nameInput, password: passInput, role: roleInput };
+            let client = await MongoClient.connect(url);
+            let dbo = client.db("toyshop");
+            await dbo.collection("users").insertOne(newUser);
+            res.redirect('/login')
+     }
+})
+//////////////////////////
+//login
+app.get('/login', (req, res) => {
+    res.render('login')
 })
 app.post('/doLogin', async (req, res) => {
     var nameInput = req.body.txtName;
@@ -61,11 +73,10 @@ app.post('/doLogin', async (req, res) => {
     let dbo = client.db("toyshop");
     const cursor = dbo.collection("users").
         find({ $and: [{ name: nameInput }, { password: passInput }] });
-
-    const count = await cursor.count();
+    const count = await cursor.count();    
 
     if (count == 0) {
-        res.render('login', { message: 'Invalid user!' })
+        res.render('login', {geterr: 'Wrong account please try again !!!'})
     } else {
         let name = '';
         let role = ''
@@ -83,7 +94,8 @@ app.post('/doLogin', async (req, res) => {
         else { res.redirect('/') }
     }
 })
-
+//////////////////////////////
+//index off admin page
 app.get('/allProduct', async (req, res) => {
     var user = req.session.User;
     let client = await MongoClient.connect(url, { useUnifiedTopology: true });
@@ -96,8 +108,8 @@ app.get('/allProduct', async (req, res) => {
     }
 })
 
-//////////////////////////////////////////////////////////////////////////////////////////
-
+/////////////////////////////////
+//indax off user page
 app.get('/', async (req, res) => {
     var user = req.session.User;
     let client = await MongoClient.connect(url, { useUnifiedTopology: true });
@@ -109,8 +121,8 @@ app.get('/', async (req, res) => {
         res.render('index', { name: user.name, role: user.role, model: results })
     }
 })
-
-
+//////////////////////////////////
+//delete function
 app.get('/delete', async (req, res) => {
     let inputId = req.query.id;
     let client = await MongoClient.connect(url);
@@ -120,18 +132,14 @@ app.get('/delete', async (req, res) => {
     await dbo.collection("products").deleteOne(condition);
     res.redirect('/allProduct');
 })
-
+/////////////////
+//Cart page
 app.get('/Cart', async (req, res) => {
-    var user = req.session.User;
-    let client = await MongoClient.connect(url, { useUnifiedTopology: true });
-    let dbo = client.db("toyshop");
-    let result = await dbo.collection("category").find({}).sort({ Name: -1 }).toArray();
-    if (!user || user.name == '') {
-        res.render('notLogin', { message: 'user chua dang nhap' })
-    } else {
-        res.render('Cart', { name: user.name, role: user.role, model: result })
-    }
+    res.render('Cart')
 })
+
+/////////////////
+//insert product for admin page
 app.get('/Insert', async (req, res) => {
     var user = req.session.User;
     let client = await MongoClient.connect(url, { useUnifiedTopology: true });
@@ -146,17 +154,8 @@ app.get('/Insert', async (req, res) => {
             res.render('Insert', { name: user.name, role: user.role })
         }
     }
-
 })
-app.get('/Moreabout', (req, res) => {
-    res.render('Moreabout.hbs');
-})
-app.get('/Contact', (req, res) => {
-    res.render('Contact.hbs');
-})
-app.get('/user', (req, res) => {
-    res.render('user.hbs');
-})
+//insert function
 app.post('/doInsert', async (req, res) => {
     let inputName = req.body.txtName;
     let inputID = req.body.txtID;
@@ -174,7 +173,8 @@ app.post('/doInsert', async (req, res) => {
         res.redirect('/allProduct');
     }
 })
-
+///////////////////////
+//sert function
 app.post('/doSearch', async (req, res) => {
     let inputName = req.body.txtName;
     let client = await MongoClient.connect(url);
@@ -183,7 +183,16 @@ app.post('/doSearch', async (req, res) => {
     res.render('allProduct', { model: results });
 
 })
-
+///////////////////////
+//some other page
+app.get('/Moreabout', (req, res) => {
+    res.render('Moreabout.hbs');
+})
+app.get('/Contact', (req, res) => {
+    res.render('Contact.hbs');
+})
+///////////////////////
+//update page
 app.get('/update', async (req, res) => {
     let id = req.query.id;
     console.log(id)
@@ -194,6 +203,7 @@ app.get('/update', async (req, res) => {
     let results = await dbo.collection("products").find(condition).toArray();
     res.render('update', { model: results });
 })
+//upadte function
 app.post('/doupdate', async (req, res) => {
     let id = req.body.id;
     var ObjectID = require('mongodb').ObjectID;
@@ -212,9 +222,11 @@ app.post('/doupdate', async (req, res) => {
     await dbo.collection("products").updateOne(condition, change);
     res.redirect('/allProduct');
 })
+//////////////////
 
-
+//run server
 const PORT = process.env.PORT || 5000
-
 app.listen(PORT);
 console.log('Running at port 5000')
+/////////////
+//end
